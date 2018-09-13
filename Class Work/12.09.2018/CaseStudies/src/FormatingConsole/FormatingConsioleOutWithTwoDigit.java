1c52353b5f6556b62a113f41fb2fceb568589e69
Mohammad Rajaul Islam <springapidev@gmail.com>
2018-09-13 13:13:54 +0600

    kdfjksjdfkj

--- Class Work/12.09.2018/CaseStudies/src/FormatingConsole/FormatingConsioleOutWithTwoDigit.java
+++ Class Work/12.09.2018/CaseStudies/src/FormatingConsole/FormatingConsioleOutWithTwoDigit.java
@@ -2,12 +2,12 @@
 package FormatingConsole;
 
 
-public class FormatingConsoleOutput {
+public class FormatingConsioleOutWithTwoDigit {
     public static void main(String[] args) {
         double amount = 12618.98;
         double interestRate = 0.0013;
         double interest = amount * interestRate;
-        System.out.println("Interest is $ " + interest);
+        System.out.println("Interest is $ " + (int) (interest * 100) /100.0);
     }
     
 }
---
+++ Class Work/13.09.2018/MethodOfJava/src/NestSum.java
@@ -0,0 +1,14 @@
+
+public class NestSum {
+    public static void main(String[] args) {
+     NestSum m = new NestSum();
+        int bigSum = m.makeSum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        System.out.println("Sum: " + bigSum);
+        
+    }
+    public int makeSum(int n1, int n2, int n3, int n4, int n5, int n6, int n7, int n8, int n9, int n10){
+        int sum =  n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10;
+        return sum;
+}
+    
+}
