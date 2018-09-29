caf00946dfdae16145f268f2d8921f322a1340d8
unknown <jubayirhossain4448@gmail.com>
2018-09-29 23:26:11 +0600

    ksdjfiuio

---
+++ Home Work/26.09.2018/MCQ/src/McqTest/Test.java
@@ -0,0 +1,15 @@
+
+package McqTest;
+
+
+public class Test {
+    public static void main(String args[]){
+                int i = 0, j = 5 ;
+                for( ; (i < 3) && (j++ < 10) ; i++ ){
+                        System.out.print(" " + i + " " + j );
+                }
+                System.out.print(" " + i + " " + j );
+        }
+
+    
+}
---
+++ Home Work/26.09.2018/Module4Evidence/src/Test.java
@@ -0,0 +1,10 @@
+public class Test{
+        public static void main(String args[]){
+                int i; 
+                for(i = 1; i < 6; i++){ 
+                        if(i > 3) {
+                        } 
+                }
+                System.out.println(i); 
+        }
+}
