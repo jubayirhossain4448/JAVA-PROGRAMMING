9c615856892673ab43ffbbec7e00e4b9134eb800
unknown <jubayirhossain4448@gmail.com>
2018-10-02 16:12:41 +0600

    jfjsklfjklasdj

---
+++ Home Work/26.09.2018/MCQ/src/McqTest/TestThree.java
@@ -0,0 +1,15 @@
+
+package McqTest;
+
+
+public class TestThree {
+    public static void main(String[] args){
+            double sum = 0;
+            for(double d = 0; d < 10;){
+                  d += 0.1;
+                  sum += sum + d;
+            }
+      }
+
+    
+}
---
+++ Home Work/26.09.2018/MCQ/src/McqTest/TestTwo.java
@@ -0,0 +1,13 @@
+
+package McqTest;
+
+public class TestTwo {
+            public static void main(String args[]){
+                int i, j;
+                for(i=1, j=0; i<10;i++) j += i; 
+                System.out.println(i);
+        }
+}
+
+    
+
